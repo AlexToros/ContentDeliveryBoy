41ca9fa76c45f8f05ac22d139e3700add7359ee4
Александр <pyankov.work@gmail.com>
2019-04-25 12:27:29 +0300

    ADD Trello classes

---
+++ ContentDeliveryBoy/Trello/TrelloClient.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace ContentDeliveryBoy.Trello
+{
+    public class CardTrelloClient : IContentProducer
+    {
+        public object GetJSONContent()
+        {
+            using (WebClient wc = new WebClient())  
+            {
+                var response = wc.DownloadString(ConfigurationSettings.AppSettings["TrelloURL"]);
+                return JsonConvert.DeserializeObject<Card[]>(response);
+            }
+        }
+    }
+}
